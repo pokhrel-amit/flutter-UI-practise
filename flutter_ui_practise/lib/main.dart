bb90b1e9c970706ba129f64fef4a7417c3870700
amit pokhrel <contact.amitpok@gmail.com>
2022-09-04 00:12:30 +0545

    refact:firbase_option.dart added

--- flutter_ui_practise/lib/main.dart
+++ flutter_ui_practise/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_practise/firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
     await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
 
